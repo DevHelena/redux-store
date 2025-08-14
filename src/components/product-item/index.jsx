9d294100f69a9c3e5d285f833856b143f25c4cd5
Pedro Pedro <helenaaraujo.dev@gmail.com>
2025-08-14 17:09:02 -0400

    First commit: Completes the logical part of the redux

---
+++ src/components/product-item/index.jsx
@@ -0,0 +1,30 @@
+import { BsCartPlus } from "react-icons/bs";
+import CustomButton from "../custom-button/index";
+import * as Styles from "./styles";
+import { addProduct } from "../../redux/cart/actions";
+import { useDispatch } from "react-redux";
+
+const ProductItem = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const handleProductClick = () => {
+    dispatch(addProduct(product))
+  }
+
+  return (
+    <Styles.ProductContainer>
+      <Styles.ProductImage imageUrl={product.imageUrl}>
+        <CustomButton startIcon={<BsCartPlus />} onClick={handleProductClick}>
+          Adicionar ao carrinho
+        </CustomButton>
+      </Styles.ProductImage>
+
+      <Styles.ProductInfo>
+        <p>{product.name}</p>
+        <p>R${product.price}</p>
+      </Styles.ProductInfo>
+    </Styles.ProductContainer>
+  );
+};
+
+export default ProductItem;
---
+++ src/redux/cart/action-types.js
@@ -0,0 +1,9 @@
+const CartActionTypes = {
+    ADD_PRODUCT: "product/add",
+    REMOVE_PRODUCT: "product/remove",
+    CLEAR_CART: "product/clear",
+    INCREASE_PRODUCT: "product/increase",
+    DECREASE_PRODUCT: "product/decrease",
+}
+
+export default CartActionTypes
