c029dd75f340db0473826243ee8b7ea512c1bd35
Pedro Pedro <helenaaraujo.dev@gmail.com>
2025-08-15 12:02:35 -0400

    add comment about redux-logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,18 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-//import logger from "redux-logger";
 import rootReducer from "./rootReducer";
+// import logger from "redux-logger"; 
+// ↑ Uncomment this line and the applyMiddleware line below to enable Redux Logger.
+// Redux Logger intercepts all dispatched actions and logs to the console:
+// - The previous state
+// - The dispatched action
+// - The next state
+// This is very useful for debugging during development, 
+// but should not be used in production because it can clutter the console 
+// and potentially expose sensitive data.
 
-const enhancer = undefined //applyMiddleware(logger);
+const enhancer = undefined; 
+// To enable Redux Logger, replace this with:
+// const enhancer = applyMiddleware(logger);
 
 const store = createStore(rootReducer, undefined, enhancer);
 
